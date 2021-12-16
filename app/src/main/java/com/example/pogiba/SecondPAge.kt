4ddc41a81b9b7e9ff992054e926cd658edce9c0b
Vanessa <pogiba_v@auca.kg>
2021-12-16 14:53:24 +0600

    Vanessa

---
+++ app/src/main/java/com/example/pogiba/SecondPAge.kt
@@ -0,0 +1,11 @@
+package com.example.pogiba
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SecondPAge : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second_page)
+    }
+}
